6f427f105db74f2b9aa2c69432fcaed86a7b9a50
abdelghaniomar <attafighani@gmail.com>
2024-03-22 12:35:46 +0100

    orcid checksum fix

--- dome-registry-ws/src/review/dto/submit-wizard.dto.ts
+++ dome-registry-ws/src/review/dto/submit-wizard.dto.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty, Validate } from "class-validator";
 import { CreateReviewDto } from "./create-review.dto";
 import { OrcidChecksum } from "../orcid-cheksum.service";
+import { ValidateNested } from 'class-validator';
 
 
 export class ReviewUser {
@@ -14,18 +15,21 @@ export class ReviewUser {
 
 export class SubmitWizards {
 
-    
+    @ValidateNested()
     @IsNotEmpty()
     user: ReviewUser;
 
- 
+    @ValidateNested()
     @IsNotEmpty()
     review: CreateReviewDto
 
 }
 
 export class ReviewSubmission {
-
+    @ValidateNested()
     @IsNotEmpty()
     ReviewUser: SubmitWizards
 }
+
+
+
