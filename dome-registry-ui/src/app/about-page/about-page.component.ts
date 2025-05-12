1511fef8c3e8527c82fbc2ee3e8dac0b1489d4ea
Ramin Khalili <ramin.khalili@studenti.unipd.it>
2025-05-12 11:18:26 +0200

    first checkpoint for about page

--- dome-registry-ui/src/app/about-page/about-page.component.ts
+++ dome-registry-ui/src/app/about-page/about-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about-page',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
